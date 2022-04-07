ae10b10557afb5824a36aca74efb8581d52a82f4
agent47daun <agent47daun@gmail.com>
2022-04-07 16:23:23 +0400

    Аргументы для клиента

--- client.py
+++ client.py
@@ -1,11 +1,45 @@
 import socket
+import argparse
+import pickle
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 IP = "localhost"
 PORT = 5000
-connection.connect((IP, PORT))
-connection.send("И тебе привет!".encode('utf8'))
-rd = connection.recv(1024)
-print(type(rd))
-print(rd.decode())
-connection.close()
+
+
+def init_argparse():
+    parser = argparse.ArgumentParser(
+        usage="%(prog)s [OPTION]...",
+    )
+    parser.add_argument("-t", "--task",
+                        help="ID задания",
+                        required=True
+                        )
+    parser.add_argument("-d", "--data",
+                        help="Строка для задания",
+                        required=True
+                        )
+    # parser.add_argument('-i', '--interactive',
+    #                     help="Интерактивный режим(Пакетный)")
+    return parser
+
+
+def main():
+    parser = init_argparse()
+    args = parser.parse_args()
+    task_id = args.task
+    data = args.data
+
+    connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    connection.connect((IP, PORT))
+
+    request_data = pickle.dumps([task_id, data])
+
+    connection.send(request_data)
+    rd = connection.recv(4096)
+    print(pickle.loads(rd))
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
